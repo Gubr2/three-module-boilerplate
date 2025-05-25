2a3ca81428c0bf288c000bb8890011ce8aa5f452
gubr2 <gubrica.a@gmail.com>
2025-05-25 18:17:13 +0200

    coommmit

--- src/modules/World/Geometry/Particles.js
+++ src/modules/World/Geometry/Particles.js
@@ -1,31 +1,49 @@
 import * as THREE from 'three/webgpu'
-import { uv, vec4, attributeArray } from 'three/tsl'
+import { uv, vec4, attributeArray, hash, instanceIndex, instancedArray, Fn } from 'three/tsl'
 import Gl from '../../Gl'
 
 export default class Particles {
   constructor() {
     this.gl = new Gl()
 
+    this.count = 10000
+
+    this.positionBuffer = instancedArray(this.count, 'vec3')
+
+    this.computeInit = Fn(() => {
+      const position = this.positionBuffer.element(instanceIndex)
+
+      const randX = hash(instanceIndex)
+      const randY = hash(instanceIndex.add(2))
+      const randZ = hash(instanceIndex.add(3))
+
+      position.x = randX.mul(100).add(-50)
+      position.y = 0 // randY.mul( 10 );
+      position.z = randZ.mul(100).add(-50)
+
+      // color.assign(vec3(randX, randY, randZ))
+    })().compute(this.count)
+
+    this.geometry = new THREE.BufferGeometry()
+    this.geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(3), 3)) // single vertex ( not triangle )
+    this.geometry.drawRange.count = 1 // force render points as instances ( not triangle )
+
+    this.material = new THREE.PointsNodeMaterial({
+      size: 0.1,
+      sizeAttenuation: true,
+      depthWrite: false,
+      color: 0x000000,
+      // blending: THREE.AdditiveBlending,
+      // vertexColors: true,
+    })
+    this.material.positionNode = this.positionBuffer.element(instanceIndex)
+
     this.instance = new THREE.Mesh(
       //
-      new THREE.PlaneGeometry(1, 1),
-      new THREE.MeshBasicNodeMaterial({
-        colorNode: vec4(uv().x, uv().y, 0.0, 1.0),
-        side: THREE.DoubleSide,
-      })
+      this.geometry,
+      this.material
     )
-
-    this.count = 1000
-    this.positionsArray = new Float32Array(this.count * 3)
-
-    for (let i = 0; i < this.count; i++) {
-      this.positionsArray[i * 3] = Math.random() * 2 - 1
-      this.positionsArray[i * 3 + 1] = Math.random() * 2 - 1
-      this.positionsArray[i * 3 + 2] = Math.random() * 2 - 1
-    }
-
-    const positionStorage = attributeArray(this.positionsArray, 'vec3')
-    positionStorage.setPBO()
+    this.instance.count = this.count
   }
 
   update() {
