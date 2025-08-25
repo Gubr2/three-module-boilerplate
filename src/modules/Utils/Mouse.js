7a72a61f740516f4b12c83102de07061aa8c5708
gubr2 <gubrica.a@gmail.com>
2025-08-25 17:04:45 +0200

    fixed page mouse

--- src/modules/Utils/Mouse.js
+++ src/modules/Utils/Mouse.js
@@ -149,7 +149,7 @@ export default class Mouse {
 
       // Set Default
       this.default.x = _event.touches[0].clientX - this.bounds.left
-      this.default.y = _event.touches[0].clientY - this.bounds.top
+      this.default.y = _event.touches[0].pageY - this.bounds.top
 
       if (this.params.limitToBounds) {
         this.default.x = Math.max(0, Math.min(this.bounds.width, this.default.x))
