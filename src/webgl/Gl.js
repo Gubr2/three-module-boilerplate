d5b7907783e08f6ec07577aa094184ee92043f8a
gubr2 <gubrica.a@gmail.com>
2026-02-17 10:29:28 +0100

    added refresh

--- src/webgl/Gl.js
+++ src/webgl/Gl.js
@@ -1,5 +1,6 @@
 import WebGL from 'three/addons/capabilities/WebGL.js'
 import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { Transform, Camera } from 'ogl'
 
 import Renderer from './Renderer'
@@ -95,6 +96,8 @@ export default class Gl {
       if (!this.isLoaded) {
         this.didResizedBeforeWebglLoaded = true
       }
+
+      ScrollTrigger.refresh() // Manual refresh after everyting gets resized
     })
   }
 
