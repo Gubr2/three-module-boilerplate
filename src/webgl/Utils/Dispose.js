f494bf9b4e25da57cc24ce7b3653187916bc8643
gubr2 <gubrica.a@gmail.com>
2026-03-25 16:51:59 +0100

    save

--- src/webgl/Utils/Dispose.js
+++ src/webgl/Utils/Dispose.js
@@ -1,24 +1,20 @@
 export default class Dispose {
-  constructor() {
-
-  }
+  constructor() {}
 
   allTextures(_item) {
     if (_item?.material) {
       // Dispose default material textures
-      if (_item.material) {
-        for (const key in _item.material) {
-          const value = _item.material[key];
+      for (const key in _item.material) {
+        const value = _item.material[key]
 
-          if (value && value.isTexture) {
-            value.dispose();
-          }
+        if (value && value.isTexture) {
+          value.dispose()
         }
       }
 
       // Dispose textures in material uniforms
       if (_item.material.uniforms) {
-        Object.values(_item.material.uniforms).forEach(_uniform => {
+        Object.values(_item.material.uniforms).forEach((_uniform) => {
           if (_uniform.value && _uniform.value.isTexture) {
             _uniform.value.dispose()
           }
