b1b651ee5159e938137e53990d7864333df275f6
gubr2 <gubrica.a@gmail.com>
2026-03-27 15:37:44 +0100

    s

--- src/webgl/World/Scenes/SceneBoilerplate.js
+++ src/webgl/World/Scenes/SceneBoilerplate.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import gsap from '@/lib/gsap'
-import { ScrollTrigger } from '@/lib/gsap'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap'
 
 import Gl from '../../Gl'
 
